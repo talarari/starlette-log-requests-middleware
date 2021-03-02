367835c1cbcda4a251ef836cd8573181cd978cb1
tal.harari <tal.harari@cs-robotics.com>
2021-03-02 16:23:43 +0200

    dont care about more-body + ignore routes

--- LogRequestsMiddleware.py
+++ LogRequestsMiddleware.py
@@ -1,17 +1,30 @@
+from typing import Tuple, List, Optional
+from starlette.datastructures import Headers
 from starlette.requests import Request
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
 import json
-from logging import Logger, getLogger
+from logging import getLogger, Logger
+from starlette.routing import Match
+from dataclasses import dataclass
+
+
+@dataclass
+class IgnoredRoute:
+    path: str
+    method: Optional[str] = None
 
 
 class LogRequestsMiddleware:
-    def __init__(self, app: ASGIApp, logger: Logger = getLogger(__name__)) -> None:
+    def __init__(
+        self, app: ASGIApp, logger: Logger = getLogger(__name__), ignored_routes: List[IgnoredRoute] = []
+    ) -> None:
         self.app = app
         self.logger = logger
+        self.ignore_routes = ignored_routes
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] == "http":
-            responder = _RequestLoggingResponder(self.app, logger=self.logger)
+            responder = _RequestLoggingResponder(self.app, logger=self.logger, ignored_routes=self.ignore_routes)
             await responder(scope, receive, send)
             return
 
@@ -19,66 +32,101 @@ class LogRequestsMiddleware:
 
 
 class _RequestLoggingResponder:
-    def __init__(self, app: ASGIApp, logger: Logger) -> None:
+    def __init__(self, app: ASGIApp, logger: Logger, ignored_routes: List[IgnoredRoute]) -> None:
         self.app = app
         self.receive: Receive = unattached_receive
         self.send: Send = unattached_send
-        self.path: str = None
-        self.method: str = None
-        self.request_body = None
-        self.response_body = None
-        self.response_status_code = None
-        self.logger = logger
+        self._path: str = ""
+        self._method: str = ""
+        self._request_body: bytearray = bytearray()
+        self._response_body: bytearray = bytearray()
+        self._response_status_code = None
+        self._logger = logger
+        self._ignored_routes = ignored_routes
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         self.receive = receive
         self.send = send
 
         request = Request(scope)
-        self.method = request.method
-        self.path = request.url.path
+
+        if self._should_ignore_request(request):
+            await self.app(scope, self.receive, self.send)
+            return
+
+        self._method = request.method
+        self._path = request.url.path
+        headers = Headers(scope=scope)
+        self.should_log_request_body = "application/json" in headers.get("content-type", "")
 
         await self.app(scope, self.receive_with_logging, self.send_with_logging)
-        self.logger.info(
-            f"{self.method} {self.path} {self.response_body} -> {self.response_status_code} {self.response_body}"
-        )
+
+        self._safe_log_request_response()
 
     async def receive_with_logging(self) -> Message:
         message = await self.receive()
 
-        assert message["type"] == "http.request"
+        if message["type"] != "http.request":
+            return message
 
-        body = message["body"]
-        more_body = message.get("more_body", False)
+        if not self.should_log_request_body:
+            return message
 
-        if more_body:
-            # Some implementations (e.g. HTTPX) may send one more empty-body message.
-            # Make sure they don't send one that contains a body, or it means
-            # that clients attempt to stream the request body.
-            message = await self.receive()
-            if message["body"] != b"":  # pragma: no cover
-                raise NotImplementedError("Streaming the request body isn't supported yet")
-
-        self.request_body = body
+        body: bytes = message.get("body", b"")
+        self._request_body.extend(body)
 
         return message
 
     async def send_with_logging(self, message: Message) -> None:
 
         if message["type"] == "http.response.start":
-            self.response_status_code = message.get("status")
+            self._response_status_code = message.get("status")
+            headers = Headers(raw=message["headers"])
+            self.should_log_response_body = "application/json" in headers.get("content-type", "")
+
             await self.send(message)
 
         elif message["type"] == "http.response.body":
-            body = message.get("body", b"")
-            more_body = message.get("more_body", False)
-            if more_body:  # pragma: no cover
-                raise NotImplementedError("Streaming the response body isn't supported yet")
+            if not self.should_log_response_body:
+                await self.send(message)
+                return
 
-            body = json.loads(body)
-            self.response_body = body
+            body: bytes = message.get("body", b"")
+            self._response_body.extend(body)
 
             await self.send(message)
+
+    def _safe_log_request_response(self):
+        try:
+            request_body = self._request_body and json.loads(self._request_body)
+            response_body = self._response_body and json.loads(self._response_body)
+            self._logger.info(
+                f"request: {self._method} {self._path} {request_body or ''} ->\
+                {self._response_status_code} {response_body or ''}"
+            )
+        except Exception:
+            pass
+
+    def _should_ignore_request(self, request: Request):
+        path_tempalte, is_handled_path = self.get_path_template(request)
+        if not is_handled_path:
+            return False
+
+        for ignored_route in self._ignored_routes:
+            if path_tempalte == ignored_route.path and (
+                ignored_route.method is None or ignored_route.method.lower() == request.method.lower()
+            ):
+                return True
+        return False
+
+    @staticmethod
+    def get_path_template(request: Request) -> Tuple[str, bool]:
+        for route in request.app.routes:
+            match, child_scope = route.matches(request.scope)
+            if match == Match.FULL:
+                return route.path, True
+
+        return request.url.path, False
 
 
 async def unattached_receive() -> Message:
